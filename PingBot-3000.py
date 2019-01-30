b2b9370ac9adc890bc2d664fc33075e650ef38ac
TyrannousHail03 <TyrannousHail03@users.noreply.github.com>
2019-01-30 15:46:37 -0500

    PingBot-3000 v1.1
    Added in keys that were not programmed in.

--- PingBot-3000.py
+++ PingBot-3000.py
@@ -261,6 +261,38 @@ async def on_message(message):
     if message.content.startswith('+'):
         msg = 'Hello there, {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
+    if message.content.startswith('*'):
+        msg = 'Hello there, {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+    if message.content.startswith('&'):
+        msg = 'Hello there, {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+    if message.content.startswith('^'):
+        msg = 'Hello there, {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+    if message.content.startswith('%'):
+        msg = 'Hello there, {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+    if message.content.startswith('$'):
+        msg = 'Hello there, {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+    if message.content.startswith('#'):
+        msg = 'Hello there, {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+    if message.content.startswith('@'):
+        msg = 'Hello there, {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+    if message.content.startswith('!'):
+        msg = 'Hello there, {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+    if message.content.startswith('`'):
+        msg = 'Hello there, {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+    if message.content.startswith('~'):
+        msg = 'Hello there, {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+    # Cannot use \ in this program, due to the newline conventions
+    
     
 
 
